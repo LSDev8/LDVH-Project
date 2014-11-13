a1957187b17b472cc47b3061505f908510aa204c
BlueeTheReed <Jonathan.Sid-Otmane@etu.upmc.fr>
2014-11-13 17:51:12 +0100

    [ADD] Interface IObjets

---
+++ Develop/src/IObjets.java
@@ -0,0 +1,6 @@
+public interface IObjets{
+      public Integer creationObjet(String nom);
+      public String getNomObjet(Integer id);
+      public void setNomObjet(Integer id, String nom);
+      public void supprimerObjet(Integer id);
+}
