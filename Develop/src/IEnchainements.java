235aef6fb84f7f4ee2f0ec574b444cf32a47d33c
BlueeTheReed <Watabanga@gmail.com>
2014-11-27 18:30:46 +0100

    [MOD] oups?

--- Develop/src/IEnchainements.java
+++ Develop/src/IEnchainements.java
@@ -1,4 +1,4 @@
-public class IEnchainements{
+public interface IEnchainements{
 	public Integer creationEnchainement(Integer idSectSec, Integer idSectDest);
 	public void setTextEnchainement(Integer id, String text);
 	public void supprimerEnchainement(Integer id);
