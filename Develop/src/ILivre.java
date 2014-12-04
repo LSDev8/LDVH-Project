e12ab0c1948450519fe05c62394a2f2cb0cbc064
BlueTheReed <Watabanga@gmail.com>
2014-12-04 13:08:20 +0100

    [MOD] Gestion de la sauvegarde de Livre

--- Develop/src/ILivre.java
+++ Develop/src/ILivre.java
@@ -1,7 +1,10 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public interface ILivre {
     public void creerLivre (String nom, String chemin) ;
-    public void ouvrirChemin (String chemin) ;
-    public void sauvegarderLivre () ;
+    public void ouvrirChemin (String chemin) throws IOException ;
+    public void sauvegarderLivre () throws IOException ;
     public void supprimerLivre () ;
     public ISections getSections () ;
     public IEnchainements getEnchainements () ;
--- Develop/src/Livre.java
+++ Develop/src/Livre.java
@@ -1,18 +1,32 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectStreamException;
 import java.io.Serializable;
-import java.util.ArrayList;
 
-public class Livre implements ILivre {
-	
+public class Livre implements ILivre, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String titre, chemin;
 	private Sections sections;
 	private Enchainements ench;
+
+	//Sauvegarde
+	private ObjectOutputStream out;
+	private ObjectInputStream in;
 
 	public void creerLivre(String nom, String chemin){
 
 		this.chemin = chemin;
 		titre = nom;
 
-		sections  = new Sections();
+		sections  = Sections.createSections();
 		ench = new Enchainements();
 	}
 
@@ -26,7 +40,7 @@ public class Livre implements ILivre {
 
 	public IObjets getObjets(){
 
-		Objets o;
+		Objets o = new Objets();
 
 		for(Enchainement e : ench){
 			o.addAll(e.getObjets());
@@ -37,15 +51,59 @@ public class Livre implements ILivre {
 
 
 	//TODO: + Rendre la classe sérializable
-	public void ouvrirChemin(String chemin){
-		return ;
+	public void ouvrirChemin(String chemin) throws IOException{
+		FileInputStream fileIN = new FileInputStream(chemin);
+		in = new ObjectInputStream(fileIN);
+
+		try {
+			readObject(in);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new IOException();
+		}
+		in.close();
+		in = null;
+		fileIN.close();
 	}
-	
-	public void sauvegarderLivre(){
+
+	public void sauvegarderLivre() throws IOException{
+		FileOutputStream fileOut = new FileOutputStream(chemin);
+		out = new ObjectOutputStream(fileOut);
+
+		writeObject(out);
+
+		out.close();
+		out = null;
+		fileOut.close();
 		return ;
 	}
 
 	public void supprimerLivre(){
+		File f = new File(chemin);
+		f.delete();
+
 		return ;
 	}
+	//On lit et on écrit dans l'ordre des déclarations:
+	// titre, sections, ench
+
+	private void writeObject(java.io.ObjectOutputStream out)
+			throws IOException{
+		out.writeObject(titre);
+		out.writeObject(sections);
+		out.writeObject(ench);
+
+	}
+	private void readObject(java.io.ObjectInputStream in)
+			throws IOException, ClassNotFoundException{
+		titre = (String) in.readObject();
+		sections = (Sections) in.readObject();
+		ench = (Enchainements) in.readObject();
+	}
+	private void readObjectNoData()
+			throws ObjectStreamException{
+
+	}
+
 }
