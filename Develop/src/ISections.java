d860fdb822660ae6e9413f4242f424e8794b8a9a
sbksba <sbksba@gmail.com>
2014-12-04 16:13:45 +0100

    [DEL] destruction des parasites

--- Develop/src/ISections.java
+++
@@ -1,9 +0,0 @@
-public interface ISections{
-    public Integer creerSection();
-    public Integer setTexteSection (Integer id, String texte);
-    public String getTexteSection (Integer id);
-    public void supprimerSection (Integer id);
-    public void addObjet(Integer idSect, Integer idObj);
-    public void removeObjet(Integer id) throws UnknownObjetException;
-    public String[] listerSections();
-}
